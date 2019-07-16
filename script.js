6251ebc488b04e62ec051584ffc6fe4c0952f3a5
Maxtix <mr.kaungmyat2014myanmar@gmail.com>
2019-07-16 14:34:06 +0630

    Add new files

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function isChecked(){
     var c = document.getElementById("gridCheck").checked;
     if(!c)
     {
-       
+       alert("You haven't agreed to our terms and conditions.");
     }
 }
