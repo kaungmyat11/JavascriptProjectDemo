d7ee2536c3c7ea2a9f4effa46c1d5b899426059b
Maxtix <mr.kaungmyat2014myanmar@gmail.com>
2019-07-22 21:43:46 +0630

    Update

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function isChecked(){
-    var c = document.getElementById("gridCheck").checked;
-    if(!c)
-    {
-       alert("You haven't agreed to our terms and conditions.");
-    }
-}
+// function isChecked(){
+//     var c = document.getElementById("gridCheck").checked;
+//     if(!c)
+//     {
+//        alert("You haven't agreed to our terms and conditions.");
+//     }
+// }
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
